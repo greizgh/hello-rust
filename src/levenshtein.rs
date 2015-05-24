23f742786efb84cc56a8d2de77c34d0b0d4bd149
Greizgh <greizgh@ephax.org>
2015-05-24 09:49:54 +0200

    Improve levenshtein, thank you /r/rust!

--- src/levenshtein.rs
+++ src/levenshtein.rs
@@ -2,6 +2,7 @@ use std::cmp;
 
 pub fn levenshtein(word1: &str, word2: &str) -> i32 {
     //! Compute the levenshtein distance between two words
+    
     let m = word1.len();
     let n = word2.len();
 
@@ -10,18 +11,10 @@ pub fn levenshtein(word1: &str, word2: &str) -> i32 {
     for i in 0..m+1 { d[i][0] = i as i32; }
     for j in 0..n+1 { d[0][j] = j as i32; }
 
-    for i in 1..m+1 {
-        for j in 1..n+1 {
-
-            let mut cost;
-
-            if word1.chars().nth(i-1).unwrap() == word2.chars().nth(j-1).unwrap() {
-                cost = 0;
-            } else {
-                cost = 1;
-            }
-
-            d[i][j] = cmp::min(cmp::min(d[i-1][j]+1, d[i][j-1]+1), d[i-1][j-1]+cost);
+    for (i, c1) in word1.chars().enumerate() {
+        for (j, c2) in word2.chars().enumerate() {
+            let cost = if c1 == c2 {0} else {1};
+            d[i+1][j+1] = cmp::min(cmp::min(d[i][j+1]+1, d[i+1][j]+1), d[i][j]+cost);
         }
     }
 
